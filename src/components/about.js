f69d04e0974c9d9892bd0fe185a248462dfdee54
Brendan Leonard <bmleonard33@gmail.com>
2017-12-06 00:33:11 -0500

    resize about for xs/mobile

--- src/components/about.js
+++ src/components/about.js
@@ -4,10 +4,10 @@ import { Image } from 'react-bootstrap';
 
 export const About = (props) => (
   <div className="col-xs-12">
-    <div className="col-xs-4 col-sm-3 col-md-2">
+    <div className="col-xs-12 col-sm-3 col-md-2">
       <Image src='headshot.jpeg' responsive circle/>
     </div>
-    <div className="col-xs-8 col-sm-9 col-md-10">
+    <div className="col-xs-12 col-sm-9 col-md-10">
       <h3 style={appStyle.subheader}>
         Hi, I'm Brendan.
       </h3>
